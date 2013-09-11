5c02db1053122aabffb42ff3f84c2964e1c9275c
aphilippakis <anthony@genomebridge.org>
2013-09-11 17:17:08 -0400

    real commit to develpment1 branch

--- src/Git_test_v2.java
+++ src/Git_test_v2.java
@@ -1,9 +1,15 @@
+import javax.swing.*;
 
 public class Git_test_v2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World on development branch");
+		JFrame frame = new JFrame("Hello Java!");
+		JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
+		frame.getContentPane().add(label);
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		
 	}
 
 }
