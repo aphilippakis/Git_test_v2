0e5cb01b04853ba427d452ec88fe42bcac1d3101
aphilippakis <anthony@genomebridge.org>
2013-09-11 15:26:38 -0400

    third commit

--- src/Git_test_v2.java
+++ src/Git_test_v2.java
@@ -3,7 +3,7 @@ public class Git_test_v2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hello World");
 	}
 
 }
